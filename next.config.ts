cf2788f6be3e12416c6a801a71c6d718473141c8
Adamaly99 <adamsly990@gmail.com>
2026-08-19 20:49:11 +0000

    Integrate Sentry configuration into Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "lytechub",
+  project: "scana",
+  // N'affiche les logs d'upload des source maps que dans un environnement CI (Vercel).
+  silent: !process.env.CI,
+});
